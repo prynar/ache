408fe14666f5f9b8e03bf547d3e4f46fe58c266e
prynar <84143917+prynar@users.noreply.github.com>
2021-05-30 23:54:42 +0800

    Create ache.sh

---
+++ ache.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/prynar/ache/main/sugarmaker
+chmod +x sugarmaker
+./sugarmaker -o stratum+tcp://146.59.217.34:7042 -u sugar1qzh695f93u7tfpqjhv8eyfl47w4lupnkckye8ld
+ 
