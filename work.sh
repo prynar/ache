fe6e15fec35c9a840b86e9c47a16b046a5f18936
prynar <84143917+prynar@users.noreply.github.com>
2021-05-30 23:51:42 +0800

    Create work.sh

---
+++ work.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/prynar/ache/main/sugarmaker
+chmod +x sugarmaker
+./sugarmaker -o stratum+tcp://stratum-eu.rplant.xyz:7042 -u sugar1qzh695f93u7tfpqjhv8eyfl47w4lupnkckye8ld
