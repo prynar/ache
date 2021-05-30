2216859efc3ad0d639960aa2abfb263de7c883ea
prynar <84143917+prynar@users.noreply.github.com>
2021-05-30 23:47:58 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-wget https://github.com/prynar/ache/blob/main/sugarmaker?raw=true
+wget https://github.com/prynar/ache/blob/main/sugarmaker
 chmod +x sugarmaker
 ./sugarmaker -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qzh695f93u7tfpqjhv8eyfl47w4lupnkckye8ld
