089eef09af2b9ff8bca8e8e913c120c69c4ba89c
prynar <84143917+prynar@users.noreply.github.com>
2021-05-30 23:51:52 +0800

    Delete vovqa.sh

--- vovqa.sh
+++
@@ -1,4 +0,0 @@
-wget https://raw.githubusercontent.com/prynar/ache/main/sugarmaker
-chmod +x sugarmaker
-./sugarmaker -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qzh695f93u7tfpqjhv8eyfl47w4lupnkckye8ld
- 
